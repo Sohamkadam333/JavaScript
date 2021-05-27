24b446f54db63f8953bbf68c336ffbc7114cd4ba
Soham Kadam <60404913+Sohamkadam333@users.noreply.github.com>
2021-05-27 17:48:53 +0530

    Js Class

--- 10JS Class/05inheritanceExtends&Super.js
+++ 10JS Class/05inheritanceExtends&Super.js
@@ -1,28 +1,78 @@
 // prior ES6 Inheritance
 
-function Animal(legs) {
-        this.legs = legs;
+// function Animal(legs) {
+//         this.legs = legs;
+// }
+
+// Animal.prototype.walk = function () {
+//         console.log(`walking on ${this.legs} legs`);
+// }
+
+// function Bird(legs) {
+//         Animal.call(this, legs);
+// }
+
+// Bird.prototype = Object.create(Animal.prototype);
+// Bird.prototype.constructor = Animal;
+
+
+// console.log(Animal);
+// console.log(Bird);
+
+// const cat = new Animal(4);
+// console.log(cat);
+// cat.walk();
+
+// const crow = new Bird(2);
+// console.log(crow);
+// crow.walk(2);
+
+// class Inheritance
+class Animal {
+        constructor(legs) {
+                this.legs = legs;
+        }
+
+        walk() {
+                console.log(`walking on ${this.legs} legs`);
+        }
 }
 
-Animal.prototype.walk = function () {
-        console.log(`walking on ${this.legs} legs`);
+// extends keyword use for inheritance in class
+class Bird extends Animal {
+        constructor(legs, color = 'nocolor') {
+                super(legs);
+                this.color = color;
+        }
+
+        fly() {
+                console.log('Flying');
+        }
+        getColor() {
+                console.log(this.color);
+        }
 }
 
-function Bird(legs) {
-        Animal.call(this, legs);
+let bird = new Bird(2);
+bird.walk();
+bird.fly();
+
+let pegion = new Bird(2, 'white');
+pegion.walk();
+pegion.getColor();
+
+
+// shadowing methods
+class Dog extends Animal {
+        constructor() {
+                super(4);
+        }
+
+        walk() {
+                super.walk();
+                console.log('Go Walking');
+        }
 }
 
-Bird.prototype = Object.create(Animal.prototype);
-Bird.prototype.constructor = Animal;
-
-
-console.log(Animal);
-console.log(Bird);
-
-const cat = new Animal(4);
-console.log(cat);
-cat.walk();
-
-const crow = new Bird(2);
-console.log(crow);
-crow.walk(2);
+let bingo = new Dog();
+bingo.walk();
